5a99fcf84516399889250849b1e66f1da0e2bde8
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-19 02:55:50 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ require_relative "./student.rb"
 
 class ChattyStudent < Student
   def hello 
-    chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    chatty_phrase = "\n How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     
     super.to_s + chatty_phrase
   end
