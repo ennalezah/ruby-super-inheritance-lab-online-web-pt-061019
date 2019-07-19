e7cf1879db1d0303d19251ab3041f66ec659de01
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-19 02:36:10 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,7 @@ class ChattyStudent < Student
     "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
-  def raise_hand
+  def raise_hand(10)
     super(10)
   end
 end
